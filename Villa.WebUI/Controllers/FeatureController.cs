2148cebe8630cf9cdd6dd08325fb535ba145f1af
Erhan Gündüz <erhangndz@live.com>
2024-02-27 20:33:22 +0300

    25. ders sonu

---
+++ Villa.WebUI/Controllers/FeatureController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.DealDtos;
+using Villa.Dto.Dtos.FeatureDtos;
+using Villa.Entity.Entities;
+
+namespace Villa.WebUI.Controllers
+{
+    public class FeatureController : Controller
+    {
+        private readonly IFeatureService _featureService;
+        private readonly IMapper _mapper;
+
+        public FeatureController(IFeatureService featureService, IMapper mapper)
+        {
+            _featureService = featureService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _featureService.TGetListAsync();
+            var valueList = _mapper.Map<List<ResultFeatureDto>>(values);
+            return View(valueList);
+        }
+
+        public async Task<IActionResult> DeleteFeature(ObjectId id)
+        {
+            await _featureService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreateFeature()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var newValue = _mapper.Map<Feature>(createFeatureDto);
+            await _featureService.TCreateAsync(newValue);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> UpdateFeature(ObjectId id)
+        {
+            var value = await _featureService.TGetByIdAsync(id);
+            var updateValue = _mapper.Map<UpdateFeatureDto>(value);
+            return View(updateValue);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var updateValue = _mapper.Map<Feature>(updateFeatureDto);
+            await _featureService.TUpdateAsync(updateValue);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Villa.WebUI/Controllers/MessageController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.DealDtos;
+using Villa.Dto.Dtos.MessageDtos;
+using Villa.Entity.Entities;
+
+namespace Villa.WebUI.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly IMessageService _messageService;
+        private readonly IMapper _mapper;
+
+        public MessageController(IMessageService messageService, IMapper mapper)
+        {
+            _messageService = messageService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _messageService.TGetListAsync();
+            var valueList = _mapper.Map<List<ResultMessageDto>>(values);
+            return View(valueList);
+        }
+
+        public async Task<IActionResult> DeleteMessage(ObjectId id)
+        {
+            await _messageService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreateMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var newValue = _mapper.Map<Message>(createMessageDto);
+            await _messageService.TCreateAsync(newValue);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> MessageDetails(ObjectId id)
+        {
+            var value = await _messageService.TGetByIdAsync(id);
+            var messageValue = _mapper.Map<ResultMessageDto>(value);
+            return View(messageValue);
+        }
+
+    
+    }
+}
