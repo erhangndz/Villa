2b04268e0b9779138cc949e4266867b34cee1717
Erhan Gündüz <erhangndz@live.com>
2024-03-10 13:39:47 +0300

    updated

---
+++ Villa.WebUI/ViewComponents/Default-Index/_DefaultDeal.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.DealDtos;
+
+namespace Villa.WebUI.ViewComponents.Default_Index
+{
+    public class _DefaultDeal: ViewComponent
+    {
+        private readonly IDealService _dealService;
+        private readonly IMapper _mapper;
+
+        public _DefaultDeal(IDealService dealService, IMapper mapper)
+        {
+            _dealService = dealService;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _dealService.TGetListAsync();
+            var dealList = _mapper.Map<List<ResultDealDto>>(values);
+            return View(dealList);
+        }
+    }
+}
---
+++ Villa.WebUI/ViewComponents/Default-Index/_DefaultProduct.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.ProductDtos;
+
+namespace Villa.WebUI.ViewComponents.Default_Index
+{
+    public class _DefaultProduct : ViewComponent
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public _DefaultProduct(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _productService.TGetListAsync();
+            var products= _mapper.Map<List<ResultProductDto>>(values);
+            return View(products);
+        }
+    }
+}
